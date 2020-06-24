36d4bf0c50b6815fef1521d27d0454513116ae5d
arifhossain123 <arifhossain42136@gmail.com>
2020-06-24 22:03:32 +0600

    Updates

---
+++ precache-manifest.f8886f26ce72f95cc09387ecacd2b6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83aa7fb5230cb21f94b99e506a70a67",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "a1f11a836d3fe2a7deef",
+    "url": "/Quiz-App/static/css/main.876a3fb4.chunk.css"
+  },
+  {
+    "revision": "431e44eca0f0169fbbc7",
+    "url": "/Quiz-App/static/js/2.9f25e938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-App/static/js/2.9f25e938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f11a836d3fe2a7deef",
+    "url": "/Quiz-App/static/js/main.233e36b2.chunk.js"
+  },
+  {
+    "revision": "99ee6101f3be90d49e3c",
+    "url": "/Quiz-App/static/js/runtime-main.09c236d6.js"
+  }
+]);
